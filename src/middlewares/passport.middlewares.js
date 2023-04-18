f65c097b8fca9033556c49cda6d4b2a88e7cee5e
Erwin Nahue <erwinnahue@gmail.com>
2023-04-17 21:00:43 -0500

    Update files

--- src/middlewares/passport.middlewares.js
+++ src/middlewares/passport.middlewares.js
@@ -2,3 +2,8 @@ const passport = require('passport')
 
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const options = {
+    jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'Ac4d3ml0vers'
+}
